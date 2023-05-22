0a6ec94aba9c17834692f5591e30e106cf7270ee
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-22 19:11:11 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,6 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
+secret=$(openssl rand -base64 24 | tr -d '\n' ; echo)
+
+sed -i "s~SECRET_TO_CHANGE~${secret}~g" ./docker-compose.yml
