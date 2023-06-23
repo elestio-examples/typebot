e92b21f0e5a809ee35e7957da49f63ab37aa5858
Prem chavhan <pchavhan@logiquad.com>
2023-06-23 18:13:27 +0530

    updated fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,6 +1,12 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
+mkdir -p ./s3_data;
+chown -R 1001:1001 ./s3_data;
+
+mkdir -p ./db_data;
+chown -R 1001:1001 ./db_data;
+
 secret=$(openssl rand -base64 24 | tr -d '\n' ; echo)
 
 sed -i "s~SECRET_TO_CHANGE~${secret}~g" ./docker-compose.yml
