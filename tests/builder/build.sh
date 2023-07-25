564c7477970e3e6f6c4155e5a043d64f9de53c57
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-07-25 17:02:23 +0530

    Update build.sh

--- tests/builder/build.sh
+++ tests/builder/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sed -i "s~ARG SCOPE~ARG SCOPE="builder"~g" Dockerfile
+sed -i "s~RUN npm --global install turbo~RUN npm --global install turbo@1.10.9~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/typebot-builder:latest | docker load
