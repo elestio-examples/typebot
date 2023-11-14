5f61810247cb6fb2a9d8181690d21c020d3eff5e
Prem Chavhan <Pchavhan@logiquad.com>
2023-11-14 15:25:25 +0530

    Updated fixes

--- tests/builder/build.sh
+++ tests/builder/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sed -i '55i staticPageGenerationTimeout: 500,' apps/builder/next.config.mjs
 sed -i "s~ARG SCOPE~ARG SCOPE="builder"~g" Dockerfile
-sed -i "s~RUN npm --global install turbo~RUN npm --global install turbo@1.10.9~g" Dockerfile
+#sed -i "s~RUN npm --global install turbo~RUN npm --global install turbo@1.10.9~g" Dockerfile
+sed -i '22i COPY . .' Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/typebot-builder:latest | docker load
