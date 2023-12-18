40c06e4b2564e91e3ce292e080cf33243e8f7bf8
Prem Chavhan <Pchavhan@logiquad.com>
2023-12-18 16:37:09 +0530

    Updated fixes

--- tests/builder/build.sh
+++ tests/builder/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed -i '55i staticPageGenerationTimeout: 500,' apps/builder/next.config.mjs
+sed -i '33i staticPageGenerationTimeout: 500,' apps/builder/next.config.mjs
 sed -i "s~ARG SCOPE~ARG SCOPE="builder"~g" Dockerfile
 #sed -i "s~RUN npm --global install turbo~RUN npm --global install turbo@1.10.9~g" Dockerfile
 sed -i '22i COPY . .' Dockerfile
