1c24328b999a2b27eb130010b7740435ee5e3fe2
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-07 08:27:26 +0300

    fix

--- tests/viewer/build.sh
+++ tests/viewer/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 sed -i "s~ARG SCOPE~ARG SCOPE="viewer"~g" Dockerfile
+sed -i "s~5.8.0~5.12.1~g" Dockerfile
+sed -i "s~5.12.1-37.0a83d8541752d7582de2ebc1ece46519ce72a848~5.12.0-21.473ed3124229e22d881cb7addf559799debae1ab~g" Dockerfile
 # sed -i "s~RUN npm --global install turbo~RUN npm --global install turbo@1.10.9~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/typebot-viewer:latest | docker load
