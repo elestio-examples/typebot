5225b3d92d060512099cde913bd85ca241e22485
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-08 08:52:24 +0300

    fix

--- tests/builder/build.sh
+++ tests/builder/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed -i '33i staticPageGenerationTimeout: 500,' apps/builder/next.config.mjs
+sed -i "s~const nextConfig = {~const nextConfig = {\n  staticPageGenerationTimeout: 500,~g" apps/builder/next.config.mjs
 sed -i "s~ARG SCOPE~ARG SCOPE="builder"~g" Dockerfile
 sed -i "s~5.8.0~5.12.1~g" Dockerfile
 sed -i "s~5.12.1-37.0a83d8541752d7582de2ebc1ece46519ce72a848~5.12.0-21.473ed3124229e22d881cb7addf559799debae1ab~g" Dockerfile
